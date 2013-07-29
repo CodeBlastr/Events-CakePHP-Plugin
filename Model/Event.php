2de27f7a2ac02124ee40a49fc37ab84311b32f1a
Buildrr LLC <richard@buildrr.com>
2013-07-29 16:59:01 +0000

    whitespace

--- Model/Event.php
+++ Model/Event.php
@@ -14,6 +14,7 @@ App::uses('EventsAppModel', 'Events.Model');
 class Event extends EventsAppModel {
 
 	public $name = 'Event';
+	
 	public $actsAs = array('Metable', 'Galleries.Mediable');
 	
 	public $caller = '';
